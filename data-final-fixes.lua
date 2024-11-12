f6b6596540b9c3e766ebe9e9a5982487b4e4aff8
Anthony Poschen <zanven42@gmail.com>
2024-11-13 03:38:13 +1100

    feat: intial attempt at handling chunks and worms

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -5,14 +5,30 @@ local setting_regen_mult = settings.startup["entrenched-enemies-base-regen-multi
 -- anything that can spawn units
 local spawner_type = "unit-spawner"
 for k, v in pairs(data.raw[spawner_type]) do
-	local cd = v.spawning_cooldown
-	local max_hp = v.max_health
-	local hp_regen = v.healing_per_tick
-	if cd then
-		for i, j in pairs(cd) do
-			data.raw[spawner_type][k].spawning_cooldown[i] = j / setting_spawn_mult
-		end
-	end
-	data.raw[spawner_type][k].max_health = max_hp * setting_health_mult
-	data.raw[spawner_type][k].healing_per_tick = hp_regen * setting_regen_mult
+  local cd = v.spawning_cooldown
+  local max_hp = v.max_health
+  local hp_regen = v.healing_per_tick
+  if cd then
+    for i, j in pairs(cd) do
+      data.raw[spawner_type][k].spawning_cooldown[i] = j / setting_spawn_mult
+    end
+  end
+  data.raw[spawner_type][k].max_health = max_hp * setting_health_mult
+  data.raw[spawner_type][k].healing_per_tick = hp_regen * setting_regen_mult
+end
+
+-- lets upgrade all the worms now
+local turret_type = "turret"
+for k, v in pairs(data.raw[turret_type]) do
+  if v.subgroup ~= "enemies" then
+    goto continue_turret
+  end
+  if v.impact_category ~= "organic" then
+    goto continue_turret
+  end
+  local max_hp = v.max_health
+  local hp_regen = v.healing_per_tick
+  data.raw[turret_type][k].max_health = max_hp * setting_health_mult
+  data.raw[turret_type][k].healing_per_tick = hp_regen * setting_regen_mult
+  ::continue_turret::
 end
